06a97e591fa5bf01cb741a1e87d863ab1c179271
brunobonaldi94 <brunobonaldi94@gmail.com>
2023-04-24 22:00:01 -0300

    adding controlled components in contact form and starting adding input validation

--- fe/src/components/ContactForm/index.js
+++ fe/src/components/ContactForm/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import {
   Form,
@@ -10,22 +10,68 @@ import { Select } from '../Select';
 import Button from '../Button';
 
 function ContactForm({ buttonLabel }) {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [category, setCategory] = useState('');
+  const [errors, setErrors] = useState([]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    console.log('submit');
+    console.log({
+      name, email, phone, category,
+    });
+  };
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+    if (!e.target.value) {
+      setErrors((prevState) => [
+        ...prevState,
+        { field: 'name', message: 'Nome é obrigatário' },
+      ]);
+    } else {
+      setErrors((prevState) => prevState.filter((error) => error.field !== 'name'));
+    }
+  };
+  console.log(errors);
   return (
-    <Form>
-      <FormGroup
-        error="Nome é obrigatório"
-      >
-        <Input error type="text" placeholder="Nome" />
+    <Form onSubmit={handleSubmit}>
+      <FormGroup>
+        <Input
+          type="text"
+          placeholder="Nome"
+          value={name}
+          onChange={handleNameChange}
+        />
       </FormGroup>
       <FormGroup>
-        <Input type="text" placeholder="Email" />
+        <Input
+          type="text"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </FormGroup>
+
+      <FormGroup>
+        <Input
+          type="text"
+          placeholder="Telefone"
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
+        />
       </FormGroup>
       <FormGroup>
-        <Input type="text" placeholder="Telefone" />
-      </FormGroup>
-      <FormGroup>
-        <Select>
-          <option value="1">Instagram</option>
+        <Select
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+        >
+          <option value="">Categoria</option>
+          <option value="Instagram">Instagram</option>
+          <option value="Discord">Discord</option>
         </Select>
       </FormGroup>
       <ButtonContainer>
