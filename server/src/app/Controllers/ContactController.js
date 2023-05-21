3e434fc40d272693ae1af2a6c92c81c2ddf0da40
brunobonaldi94 <brunobonaldi94@gmail.com>
2023-05-21 16:10:12 -0300

    add validation in delete contact backend for uuid

--- server/src/app/Controllers/ContactController.js
+++ server/src/app/Controllers/ContactController.js
@@ -80,8 +80,10 @@ class ContactController {
 
   async delete(request, response) {
     const { id } = request.params;
+    if (!isValidUUID(id)) {
+      return response.status(400).json({ error: 'Invalid user id' });
+    }
     const contact = await ContactsRepository.findById(id);
-
     if (!contact) {
       return response.status(404).json({ error: 'Contact Not Found!' });
     }
